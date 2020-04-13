efbc712a97a40767268d669f606108d0b42692b2
usfbelhadj <usf.belhadj@gmail.com>
2020-04-13 21:02:43 +0100

    try

--- _path.c
+++ _path.c
@@ -1,30 +1,30 @@
 #include "shellib.h"
 
-/**
-* _path - find path
-* @p : char
-* @str : string
-* Return: string.
-*/
+	/**
+	* _path - find path
+	* @p: char
+	* @str: string
+	* Return: string
+	*/
 char **_path(char **p, char *str)
 {
 	struct stat st;
 	int i = 0;
 	char *chp = malloc(sizeof(char *));
 	char **ch = malloc(sizeof(char *));
+
 	while (p[i] != NULL)
 	{
-		
 		chp = _strdup(p[i]);
-		chp = strcat(chp, "/");
-		chp = strcat(chp,str);
+		chp = _strcat(chp, "/");
+		chp = _strcat(chp, str);
 		if (stat(chp, &st) == 0)
 		{
-			*ch = _strdup(chp);       	
+			*ch = _strdup(chp);
 			break;
-    	}
-    	
+		}
 		i++;
+		free(chp);
 	}
 
 	return (ch);
